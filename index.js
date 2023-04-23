0dfb964cf1d8f2466a65548d1e5ed15370dbd57a
Jessica Benson <jess.nicole.benson@gmail.com>
2023-04-23 18:40:06 -0400

    commit

--- index.js
+++ index.js
@@ -9,7 +9,20 @@ const pipe = (...fns) => firstArg => fns.reduce((returnValue, fn) => fn(returnVa
 const makeTag = tag => str => `<${tag}>${str}</${tag}>`
 
 // complete this function
-const makePoemHTML = () => {}
+const makePoemHTML = (poemJson) => {
+  const makeStanzaHTML = (stanzaLines) => {
+    return `<p>${stanzaLines.join("<br/>")}</p>`
+  }
+  const stanzas = [[]];
+  for (let line of poemJson[0].lines) {
+    if (line === "") {
+      stanzas.push([]);
+    } else {
+      stanzas[stanzas.length - 1].push(line);
+    }
+  }
+  return `<h2>${poemJson[0].title}</h2><h3><em>by ${poemJson[0].author}</em></h3>${stanzas.map(makeStanzaHTML).join("")}`
+}
 
 // attach a click event to #get-poem
 getPoemBtn.onclick = async function() {
